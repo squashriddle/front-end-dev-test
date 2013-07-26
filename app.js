ed259e9923654704b39aa28310a8f4ffb7b6eb34
Joshua <joshua@mattersofgrey.com>
2013-07-26 15:04:31 -0700

    Update app.js
    Corrections to app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var valid = true;
+var valid = false;
 
 var alpha = ["Larry", "Moe", "Curly", "Shemp"];
 var beta = ["Password1", "Password2", "Password3", "Password4"];
 
-var u = a1pha[2];
+var u = alpha[2];
 var p = beta[4];
 
-valid = false;
+valid = true;
 
-for (var i=0; i < alpha.length(); i++) {
-	i = 10;
-	if ((u == alpha[i]) && (p == beta[i])) {
+for (var i = 0; i <= alpha.length[i]; i++) {
+	i = 4;
+	if ((u === alpha[i]) && (p === beta[i])) {
 		valid = "yes";
 		break;
 	}
-}
+};
 
 if (valid) {
 	alert ("Good job on fixing that!");
@@ -22,5 +22,5 @@ if (valid) {
 }
 
 else {
-	alert ("Ops, try again!");
+	alert ("Oops, try again!");
 }
