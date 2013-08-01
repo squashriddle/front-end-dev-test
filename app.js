6edf0a380075ba42aa66a60aa86ac7a9c55cb096
squashriddle <joshua@mattersofgrey.com>
2013-07-31 20:00:20 -0700

    Modifications - Additions
    * Modified index.html
    * Corrected app.js
    * Added css/images/js

--- app.js
+++ app.js
@@ -3,22 +3,18 @@ var valid = false;
 var alpha = ["Larry", "Moe", "Curly", "Shemp"];
 var beta = ["Password1", "Password2", "Password3", "Password4"];
 
-var u = alpha[2];
-var p = beta[4];
+var u = alpha[0];
+var p = beta[0];
 
-valid = true;
-
-for (var i = 0; i <= alpha.length[i]; i++) {
-	i = 10;
+for (var i = 0; i <= alpha.length; i++) {
 	if ((u === alpha[i]) && (p === beta[i])) {
-		valid = "yes";
+		valid = true;
 		break;
 	}
 };
 
 if (valid) {
 	alert ("Good job on fixing that!");
-	return false;
 }
 
 else {
